883b4f082a727e061c469a3f39d4025881521140
PhuTam54 <phutamytb@gmail.com>
2023-03-21 17:28:50 +0700

    Phú Tâm Update HomePage

---
+++ assets/js/main.js
@@ -0,0 +1,235 @@
+const usernameEl = document.querySelector('#username')
+const firstnameEl = document.querySelector('#firstname')
+const lastnameEl = document.querySelector('#lastname')
+const emailEl = document.querySelector('#email')
+const phoneEl = document.querySelector('#phone')
+const passwordEl = document.querySelector('#password')
+const confirmPasswordEl = document.querySelector('#confirmpassword')
+
+const form = document.querySelector('#signup')
+
+// check 
+const isRequired = value => value === '' ? false : true
+const isBetween = (length, min, max) => length < min || length > max ? false : true
+
+const checkUsername = () => {
+    let valid = false
+    const min = 3, max = 25
+    const username = usernameEl.value.trim()
+    if (!isBetween(username.length, min, max)) {
+        showError(usernameEl, `User name must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(usernameEl)
+        valid = true
+    }
+    return valid
+}
+
+const checkFirstname = () => {
+    let valid = false
+    const min = 1, max = 25
+    const firstname = firstnameEl.value.trim()
+    if (!isBetween(firstname.length, min, max)) {
+        showError(firstnameEl, `First name must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(firstnameEl)
+        valid = true
+    }
+    return valid
+}
+
+const checkLastname = () => {
+    let valid = false
+    const min = 1, max = 25
+    const lastname = lastnameEl.value.trim()
+    if (!isBetween(lastname.length, min, max)) {
+        showError(lastnameEl, `Last name must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(lastnameEl)
+        valid = true
+    }
+    return valid
+}
+
+const checkEmail = () => {
+    let valid = false
+    const email = emailEl.value.trim()
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank.')
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email is not valid.')
+    } else {
+        showSuccess(emailEl)
+        valid = true
+    }
+    return valid
+}
+
+const checkPhoneNumber = () => {
+    let valid = false
+    const phone = phoneEl.value.trim()
+    const min = 10, max = 10
+
+    if (!isRequired(phone)) {
+        showError(phoneEl, 'Phone cannot be blank.')
+    } else if (!isPhoneNumber(phone)) {
+        showError(phoneEl, '10 number.')
+    } else if (!isBetween(phone.length, min, max)) {
+        showError(phoneEl, '10 number.')
+    } else {
+        showSuccess(phoneEl)
+        valid = true
+    }
+    return valid
+}
+
+const checkPassword = () => {
+    let valid = false
+
+    const password = passwordEl.value.trim()
+
+    if (!isRequired(password)) {
+        showError(passwordEl, 'Password cannot be blank.')
+    } else if (!isPasswordSecure(password)) {
+        showError(passwordEl, 'Password must has at least 8 characters that include at least 1 lowercase' +
+            'character, 1 uppercase characters, 1 number, and 1 speacial character in (!@#$%^&*)')
+    } else {
+        showSuccess(passwordEl)
+        valid = true
+    }
+
+    return valid
+}
+
+const checkConfirmPassword = () => {
+    let valid = false
+    // check confirm password
+    const confirmPassword = confirmPasswordEl.value.trim()
+    const password = passwordEl.value.trim()
+
+    if (!isRequired(confirmPassword)) {
+        showError(confirmPasswordEl, 'Please enter the password again.')
+    } else if (password !== confirmPassword) {
+        showError(confirmPasswordEl, 'The password does not match')
+    } else {
+        showSuccess(confirmPasswordEl)
+        valid = true
+    }
+
+    return valid
+}
+
+const isEmailValid = (email) => {
+    //Regular expression (check email)
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    return re.test(email)
+}
+
+const isPasswordSecure = (password) => {
+    //reguar expression (check password)
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")
+    return re.test(password)
+}
+
+const isPhoneNumber = (phone) => {
+    //reguar expression (check phone number)
+    const re = new RegExp("^(?=.*[0-9])(?=.{9,10})")
+    return re.test(phone)
+}
+
+// Show
+const showError = (input, message) => {
+    //get the form-feild element 
+    const formFeild = input.parentElement
+    //add the error class
+    formFeild.classList.remove('success')
+    formFeild.classList.add('error')
+
+    //show the error message
+    const error = formFeild.querySelector('small')
+    error.textContent = message
+
+}
+
+const showSuccess = (input) => {
+    //get the form-feild element 
+    const formFeild = input.parentElement
+    //remove the error class
+    formFeild.classList.remove('error')
+    formFeild.classList.add('success')
+
+    //hide the error message
+    const error = formFeild.querySelector('small')
+    error.textContent = ''
+}
+
+// Eventlistener
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+    e.preventDefault()
+
+    // validate fields
+    let isUsernameValid = checkUsername(),
+        isFirstnameValid = checkFirstname(),
+        isLastnameValid = checkLastname(),
+        isEmailValid = checkEmail(),
+        isPasswordValid = checkPassword(),
+        isConfirmPasswordValid = checkConfirmPassword(),
+        isPhoneNumber = checkPhoneNumber()
+
+    let isContactValid = isFirstnameValid &&
+        isLastnameValid &&
+        isEmailValid &&
+        isPhoneNumber
+
+    let isFormValid = isUsernameValid &&
+        isEmailValid &&
+        isPasswordValid &&
+        isConfirmPasswordValid &&
+        isPhoneNumber
+
+    // submit to the server if the form is valid
+    if (isContactValid) {
+        alert('Your info was send')
+    }
+})
+
+const debounce = (fn, delay = 1) => {
+    let timeoutId;
+    return (...args) => {
+        //cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId)
+        }
+        //setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay)
+    }
+}
+
+form.addEventListener('input', debounce(function (e) {
+    switch (e.target.id) {
+        case 'username':
+            checkUsername()
+            break
+        case 'firstname':
+            checkFirstname()
+            break
+        case 'lastname':
+            checkLastname()
+            break
+        case 'email':
+            checkEmail()
+            break
+        case 'password':
+            checkPassword()
+            break
+        case 'confirmpassword':
+            checkConfirmPassword()
+            break
+        case 'phone':
+            checkPhoneNumber()
+            break
+    }
+}))
