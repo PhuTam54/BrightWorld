3b87fc7183fc11de9e259b2f23ed9cebe308af31
anhduy2512033 <118426265+anhduy2512033@users.noreply.github.com>
2023-03-22 19:34:18 +0700

    sign_up

---
+++ Log-in/js/index.js
@@ -0,0 +1,27 @@
+let id=(id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
+let username = id("username");
+    password=id("password");
+    form =id("form");
+    errorMsg=classes("error");
+    successIcon=classes("success-icon");
+    failureIcon=classes("failure-icon");
+form.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    engine(username,0,"Username cannot be blank");
+    engine(password,1,"Password cannot be blank");
+});
+let engine=(id, serial, message) =>{
+    if(id.value.trim()===""){
+        errorMsg[serial].innerHTML=message;
+        id.style.border = "2px solid red";
+        failureIcon[seriall].style.opacity="1";
+        successIcon[serial].style.opacity="0";
+    }
+    else{
+        errorMsg[serial].innerHTML="";
+        id.style.border="2px solid green";
+        failureIcon[serial].style.opacity="0";
+        successIcon[serial].style.opacity="1";
+    }
+};
