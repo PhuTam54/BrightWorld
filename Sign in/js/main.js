fdc26c680a2e310b78c5cf2446bf7eb807650a68
anhduy2512033 <118426265+anhduy2512033@users.noreply.github.com>
2023-03-25 16:44:31 +0700

    update sign in

--- Sign in/js/main.js
+++ Sign in/js/main.js
@@ -2,9 +2,6 @@
 (function ($) {
     "use strict";
 
-
-    /*==================================================================
-    [ Focus input ]*/
     $('.input100').each(function(){
         $(this).on('blur', function(){
             if($(this).val().trim() != "") {
@@ -15,10 +12,7 @@
             }
         })    
     })
-  
-  
-    /*==================================================================
-    [ Validate ]*/
+
     var input = $('.validate-input .input100');
 
     $('.validate-form').on('submit',function(){
@@ -65,23 +59,21 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
-    /*==================================================================
-    [ Show pass ]*/
+ 
     var showPass = 0;
     $('.btn-show-pass').on('click', function(){
         if(showPass == 0) {
             $(this).next('input').attr('type','text');
-            $(this).addClass('active');
+            $(this).find('i').removeClass('zmdi-eye');
+            $(this).find('i').addClass('zmdi-eye-off');
             showPass = 1;
         }
         else {
             $(this).next('input').attr('type','password');
-            $(this).removeClass('active');
+            $(this).find('i').addClass('zmdi-eye');
+            $(this).find('i').removeClass('zmdi-eye-off');
             showPass = 0;
         }
         
     });
-
-
 })(jQuery);
