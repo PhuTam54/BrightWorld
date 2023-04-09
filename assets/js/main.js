01b3d8bcfb1ce4e26f60a38f15afa3ca66c8a677
PhuTam54 <phutamytb@gmail.com>
2023-04-10 00:30:58 +0700

    Add js for login

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,34 @@
+// <!-- js for modal  -->
+        const modalMain = document.querySelector('.modal');
+        const modalBody = document.querySelector('.modal__body');
+        const loginLink = document.querySelector('.login-link');
+        const registerLink = document.querySelector('.register-link');
+
+        const registerClose = document.querySelector('.register-close');
+        const loginClose = document.querySelector('.login-close');
+
+        const btnPopup = document.querySelector('.btnLogin-popup');
+
+        registerLink.addEventListener('click', () => {
+            modalBody.classList.add('active');
+        });
+
+        loginLink.addEventListener('click', () => {
+            modalBody.classList.remove('active');
+        });
+
+        btnPopup.addEventListener('click', () => {
+            modalMain.classList.add('active-popup');
+        });
+
+        registerClose.addEventListener('click', () => {
+            modalMain.classList.remove('active-popup');
+        });
+
+        loginClose.addEventListener('click', () => {
+            modalMain.classList.remove('active-popup');
+        });
+
 // VALIDATION FORM FIELDS
 const registerUsernameEl = document.querySelector('#register-username')
 const loginUsernameEl = document.querySelector('#login-username')
